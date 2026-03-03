7f1b048a47fa65697e404565eb87a81b44dbea7d
E.I.Moskvin <evgeni.moskvin@gmail.com>
2026-03-03 08:51:31 +0300

    test

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 
-from dotenv import load_dotenv
+a = input('Введите число')
+try:
+    a = int(a)
+    for i in range(a):
+        print(i)
+except Exception as e:
+    print(e)
 
-load_dotenv()
-
-from_env = os.getenv('TEST_A')
-
-print(from_env)
-
-print('dsfl;ksjdfsjldkfjfsdl')
